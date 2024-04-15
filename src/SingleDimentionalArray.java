202e5959ebfc7a6d7b22860454ad49d6cc7561f2
Snehal Popat Kadam <snehal.kadam@netcracker.com>
2024-04-16 03:23:14 +0530

    Single Dimentional Array

---
+++ src/SingleDimentionalArray.java
@@ -0,0 +1,8 @@
+public class SingleDimentionalArray {
+    public static void main(String args[]) {
+        int a[] = {6,5,3,2};
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
