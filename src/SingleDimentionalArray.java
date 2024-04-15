20bb246398e038cde54c693a510778ab0b7b943f
Snehal Popat Kadam <snehal.kadam@netcracker.com>
2024-04-16 03:28:52 +0530

    Single Dimentional Array accessed by For & Foreach loop

--- src/SingleDimentionalArray.java
+++ src/SingleDimentionalArray.java
@@ -1,8 +1,12 @@
 public class SingleDimentionalArray {
     public static void main(String args[]) {
         int a[] = {6,5,3,2};
-        for (int i = 0; i < a.length; i++) {
+        int b[] = {7,3,2};
+        for (int i = 0; i < a.length; i++) {                    //for loop
             System.out.println(a[i]);
+        }
+        for(int i: b) {                                         //for-each loop
+            System.out.println(i);
         }
     }
 }
