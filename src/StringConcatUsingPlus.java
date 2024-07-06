58fc17f181edb7de2561404f402c08631faaacd4
Snehal Popat Kadam <snehal.kadam@netcracker.com>
2024-07-06 20:47:09 +0530

    Concat String using Plus operator

---
+++ src/StringConcatUsingPlus.java
@@ -0,0 +1,5 @@
+public class StringConcatUsingPlus {
+    public static void main(String args[]) {
+        System.out.println(50+30+"Hiii"+40+20);     //After a string literal, all the + will be treated as string concatenation operator
+    }
+}
